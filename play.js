0bb82b68fe7eea7015e5ffaf772880a69b66b50d
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-01 14:49:55 -0400

    Add Handling CTRL + C User Input

--- play.js
+++ play.js
@@ -6,3 +6,31 @@ const { connect } = require('./client'); //same as const connect = require('./cl
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function (handleUserInput) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  // Event Handling for User Input to terminate the game 
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+
+  return stdin;
+}
+
+console.log(setupInput());
+
+const handleUserInput = function () {
+
+}
+
+
